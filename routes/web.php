99e74991e902212b421a25ea2177491182f254a7
SimC0de <yor1simone@outlook.com>
2024-10-22 22:44:05 +0800

    Added payroll controller and route (not finished(

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PayrollDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (){
     return view('login');
 });
+
+Route::get('/payroll_dashboard', [PayrollDashboardController::class, 'index']);
