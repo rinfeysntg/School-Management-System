cb08eb34b4baa41fe4e0d12eed82dba564ab20f7
SimC0de <yor1simone@outlook.com>
2024-10-23 17:54:53 +0800

    added a table to list employees

--- resources/views/payroll_dashboard.blade.php
+++ resources/views/payroll_dashboard.blade.php
@@ -1,5 +1,166 @@
-@extends('layout')
+@extends('payroll_layout')
 
 @section('content')
-    <h1>Hello World</h1>
+    <div class="grid-container">
+        <div class="grid-table">
+            <table class="table-list">
+                <thead>
+                    <th scope="col">#</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Surname</th>
+                    <th scope="col">Position</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">Gender</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Salary Grade</th>
+                    <th scope="col">ID#</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>Simone Roy</td>    
+                        <td>Abello</td>
+                        <td>Detective</td>
+                        <td>Police Department</td>
+                        <td>Male</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>1234146431</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="grid-slip">
+            <h1 class="pay-slip-header">Pay Slip</h1>
+            <p>Hello World</p>
+        </div>
+        <div class="grid-info">
+            <label for="fname">First name:</label>
+            <input id="fname" name="fname">
+            <br>
+            <label for="fname">First name:</label>
+            <input id="fname" name="fname">
+        </div>
+    </div>
 @endsection
