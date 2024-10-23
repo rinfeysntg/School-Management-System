ed96176cad9728c60fd4a49b71e2d043f5da492d
SimC0de <yor1simone@outlook.com>
2024-10-23 17:36:56 +0800

    Made a layout for payroll

---
+++ resources/views/payroll_layout.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/layout.css">
+    <title>Wesleyan University Philippines</title>
+    <style>
+        .payroll-container {
+            padding: 12px;
+            margin: 12px;
+        }
+        .grid-container {
+            grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+            grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+            display: grid;
+            gap: 25px;
+        }
+        .grid-table {
+            background-color: rgba(255, 255, 255, 0.8);
+            border: 1px solid rgb(255, 255, 255);
+            border-radius: 10px;
+            height: 500px;
+            grid-column-start: 1;
+            grid-column-end: 5;
+            grid-row-start: 1;
+            grid-row-end: 4;
+            overflow: scroll;
+        }
+        table.table-list {
+            border-radius: 10px;
+            color: black;
+            font-size: 25px;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        .table-list th {
+            background-color: white;
+            position: sticky;
+            top: 0;
+        }
+        .table-list td, .table-list th {
+            border: 2px solid black;
+        }
+        .grid-slip {
+            background-color: blue;
+            grid-column-start: 5;
+            grid-column-end: 6;
+            grid-row-start: 1;
+            grid-row-end: 6;
+        }
+        .grid-info {
+            background-color: yellow;
+            grid-column-start: 1;
+            grid-column-end: 2;
+            grid-row-start: 4;
+            grid-row-end: 6;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="payroll-container">
+        @yield('content')
+    </div>
+</body>
+</html>
