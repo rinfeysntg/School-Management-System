813af70cc9cf33c1883ca3b462ca67f34049e0eb
SimC0de <yor1simone@outlook.com>
2024-10-28 13:45:30 +0800

    Added date to push through payroll dashboard

--- app/Http/Controllers/PayrollDashboardController.php
+++ app/Http/Controllers/PayrollDashboardController.php
@@ -9,6 +9,7 @@ class PayrollDashboardController extends Controller
     //
     public function index()
     {
-        return view('payroll_dashboard');
+        $names = array(array('first_name' => 'Simone Roy', 'surname'=>'Abello'), array('first_name'=> 'Princess', 'surname'=>'Javier'), array('first_name'=>'Jake', 'surname'=>'The Dog'));
+        return view('payroll_dashboard')->with('names', $names);
     }
 }
