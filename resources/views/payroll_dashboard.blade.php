379c6bd6cd449cdf5e35bf9accf8260a63507cca
SimC0de <yor1simone@outlook.com>
2024-10-28 13:46:28 +0800

    Added search box and payment info, as well as looped through $names variable for more sample data

--- resources/views/payroll_dashboard.blade.php
+++ resources/views/payroll_dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="grid-table">
             <table class="table-list">
                 <thead>
-                    <th scope="col">#</th>
+                    <th scope="col">Data ID#</th>
                     <th scope="col">First Name</th>
                     <th scope="col">Surname</th>
                     <th scope="col">Position</th>
@@ -13,7 +13,7 @@
                     <th scope="col">Gender</th>
                     <th scope="col">Age</th>
                     <th scope="col">Salary Grade</th>
-                    <th scope="col">ID#</th>
+                    <th scope="col">Employee ID#</th>
                 </thead>
                 <tbody>
                     <tr>
@@ -27,6 +27,19 @@
                         <td>10</td>
                         <td>1234146431</td>
                     </tr>
+                    @foreach ($names as $name)
+                    <tr>
+                        <td scope="row">1</th>
+                        <td>{{$name["first_name"]}}</td>    
+                        <td>{{$name["surname"]}}</td>
+                        <td>...</td>
+                        <td>... ...</td>
+                        <td>.......</td>
+                        <td>...</td>
+                        <td>...</td>
+                        <td>......</td>
+                    </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
@@ -38,6 +51,9 @@
             <p>Employee ID:</p>
             <P>Full Name:</p>
             <P>Department:</p>
+            <P>Salary:</p>
+            <P>Deductions:</p>
+            <P>Total Payment:</p>
         </div>
         <form class="grid-info">
             <div class="info-first-column info-column">
@@ -51,11 +67,15 @@
                     <input id="fname" name="fname">
                 </div>
                 
-                <div class="input-box">
-                    <label for="gender">Gender:</label>
-                    <input id="gender" name="gender">
-                    <label for="age">Age:</label>
-                    <input id="age" name="age">
+                <div class="age-gender">
+                    <div class="input-gender">
+                        <label for="gender">Gender:</label>
+                        <input id="gender" name="gender">
+                    </div>
+                    <div class="input-age">
+                        <label for="age">Age:</label>
+                        <input id="age" name="age">
+                    </div>
                 </div>
                 
                 <div class="input-box">
@@ -85,7 +105,7 @@
             </div>
             <div class="info-first-column info-column">
                 <div class="input-box">
-                    <label for="fname">ID#:</label>
+                    <label for="fname">Employee ID#:</label>
                     <input id="fname" name="fname">
                 </div>
                 
@@ -105,5 +125,37 @@
                 </div>
             </div>
         </form>
+        <div class="search-salary-info">
+            <div class="search-box ssi-item">
+                <label for="search" class="search-label">Search:</label>
+                <input id="search" name="search">
+                <button>Search</button>
+            </div>
+
+            <form class="salary-info ssi-item">
+                <div class="input-box">
+                    <label for="fname">Salary:</label>
+                    <input id="fname" name="fname">
+                </div>
+                <div class="input-box">
+                    <label for="fname">Deductions:</label>
+                    <input id="fname" name="fname">
+                </div>
+                <div class="input-box">
+                    <label for="fname">Net Pay:</label>
+                    <input id="fname" name="fname">
+                </div>
+            </form>
+            <form class="payment-info ssi-item">
+                <div class="input-box">
+                    <label for="fname">Last Pay Date:</label>
+                    <input id="fname" name="fname">
+                </div>
+                <div class="input-box">
+                    <label for="fname">Next Pay Date:</label>
+                    <input id="fname" name="fname">
+                </div>
+            </form>
+        </div>
     </div>
 @endsection
