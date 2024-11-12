437ba4fddcec87f32ed577b99475621a7de9734c
Maico <123435776+Maico9921@users.noreply.github.com>
2024-11-13 07:29:58 +0800

    rooms

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoomController;
 
 Route::get('/', function (){
     return view('login');
@@ -16,3 +17,14 @@ Route::get('/create_building', function (){
     return view('registrar.create_building');
 });
 
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
+
+Route::get('/rooms/create_rooms', [RoomController::class, 'create'])->name('rooms.create');
+
+Route::post('/rooms', [RoomController::class, 'store'])->name('rooms.store');
+
+Route::get('/rooms/{id}/edit', [RoomController::class, 'edit'])->name('rooms.edit');
+
+Route::put('/rooms/{id}', [RoomController::class, 'update'])->name('rooms.update');
+
+Route::delete('/rooms/{id}', [RoomController::class, 'destroy'])->name('rooms.destroy');
