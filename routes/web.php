cab4edab90af578a908d94338d77faccc444681e
Simone Roy <160488978+SimC0de@users.noreply.github.com>
2024-11-14 16:31:10 +0800

    Revert "Payroll"

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Http\Controllers\PayrollDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (){
     return view('login');
 });
-
-Route::get('/payroll_dashboard', [PayrollDashboardController::class, 'index']);
