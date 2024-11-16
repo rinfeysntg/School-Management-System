1e688dac5ac2deb6220cfaad56e490fac31203ed
sundei2 <sund3igarcia@gmail.com>
2024-11-16 22:40:16 +0800

    REGISTRAR(test2)

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -1,5 +1,23 @@
-@extends('layout')
+@extends('layoutregistrar')
 
-@section('content')
-
+@section('registrardashboard')
+<div class="login">
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="row mb-3">
+            <label for="inputEmail" class="row-sm-3 row-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" name="email" class="form-control" id="inputEmail" required>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="inputPassword" class="row-sm-3 row-form-label">Password</label>
+            <div class="col-sm-10">
+                <input type="password" name="password" class="form-control" id="inputPassword" required>
+            </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-success">Login</button>
+    </form>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginAuth;
+use App\Http\Controllers\Registrar;
 
 Route::get('/', function (){
     return view('login');
 });
 
+// initial login funct
 Route::post('/login', [LoginAuth::class, 'Login'])->name('login');
+
+// registrar site
+Route::get('/registrar', [Registrar::class, 'index'])->name('registrar');
+
+
+
