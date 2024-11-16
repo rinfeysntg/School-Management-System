e5c61c15be345589a0329f5b6976ef9fd66d6614
GianCruz <giancruz0911@gmail.com>
2024-11-17 00:44:46 +0800

    controllers

---
+++ app/Http/Controllers/Curriculum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Curriculum extends Controller
+{
+    public function index()
+    {
+
+        return view('department');
+    }
+}
