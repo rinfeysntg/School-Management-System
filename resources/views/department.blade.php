35de24fe7dba2780f80a5166a42eda3975ffee0d
GianCruz <giancruz0911@gmail.com>
2024-11-17 00:24:57 +0800

    Department button

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -2,18 +2,23 @@
 
 @section('departmentdashboard')
 <div class="glass">
-    <h1 class="heading">Create Departmments</h1>
+    <h1 class="heading">Create Departments</h1>
     <div class="frame">
-        <div class="button-container">
-                <h4>Name:  <input></h4>
-                <br>
-                <h4>Description:  <input class="description"></h4>
-                <br>
-                <h4>Building: <select id="dropdown">
-                    <option value="option1">CECT</option>
-                    <option value="option2">CBA</option>
-                    <option value="option3">CAS</option></h4>
-        </div>
+        <h4>Name: <input></h4>
+        <br>
+        <h4>Description: <input class="description"></h4>
+        <br>
+        <h4>Building: 
+            <select id="dropdown">
+                <option value="option1">CECT</option>
+                <option value="option2">CBA</option>
+                <option value="option3">CAS</option>
+            </select>
+        </h4>
+    </div>
+    <br>
+    <div class="button-container">
+    <button type="button" class="btn">Create</button>
     </div>
 </div>
 @endsection
