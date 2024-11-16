8a326f06da5afc6ebf36f259af850e1aee847809
sundei2 <sund3igarcia@gmail.com>
2024-11-17 01:46:07 +0800

    added dashboards to each pages

--- resources/views/registrar.blade.php
+++ resources/views/registrar.blade.php
@@ -5,13 +5,13 @@
         <img src="../images/wupLogo.png" alt="meh" class="dashboardLogo">
     <div class="button-container">
         <button class="btn">Announcements</button>
-        <a href="{{ route('course') }}"><button class="btn">Create Course</button></a>
-        <a href="{{ route('curriculum') }}"><button class="btn">Create Curriculum</button></a>
+        <a href="{{ route('courseDashboard') }}"><button class="btn">Create Course</button></a>
+        <a href="{{ route('curriculumDashboard') }}"><button class="btn">Create Curriculum</button></a>
         <a href="{{ route('course') }}"><button class="btn">Buildings</button></a>
-        <a href="{{ route('department') }}"><button class="btn">Departments</button></a>
+        <a href="{{ route('departmentDashboard') }}"><button class="btn">Departments</button></a>
         <br>
         <div class="button-container-red">
-            <button class="btn">Log Out</button>
+            <a href="{{ route('/') }}"><button class="btn">Log Out</button></a>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,16 @@ use App\Http\Controllers\Registrar;
 use App\Http\Controllers\Department;
 use App\Http\Controllers\Course;
 use App\Http\Controllers\Curriculum;
+use App\Http\Controllers\LogHome;
+use App\Http\Controllers\CourseDashboard;
+use App\Http\Controllers\CurriculumDashboard;
+use App\Http\Controllers\DepartmentDashboard;
 
 Route::get('/', function (){
     return view('login');
 });
+
+Route::get('/', [LogHome::class, 'index'])->name('/');
 
 // initial login funct
 Route::post('/login', [LoginAuth::class, 'Login'])->name('login');
@@ -17,8 +23,11 @@ Route::post('/login', [LoginAuth::class, 'Login'])->name('login');
 // registrar site
 Route::get('/registrar', [Registrar::class, 'index'])->name('registrar');
 Route::get('/department', [Department::class, 'index'])->name('department');
+Route::get('/departmentdashboard', [DepartmentDashboard::class, 'index'])->name('departmentDashboard');
 Route::get('/course', [Course::class, 'index'])->name('course');
+Route::get('/coursedashboard', [CourseDashboard::class, 'index'])->name('courseDashboard');
 Route::get('/curriculum', [Curriculum::class, 'index'])->name('curriculum');
+Route::get('/curriculumdashboard', [CurriculumDashboard::class, 'index'])->name('curriculumDashboard');
 
 
 
