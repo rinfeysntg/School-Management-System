90a6f90c2fdac958de5a4acf00e16837118f52b0
sundei2 <sund3igarcia@gmail.com>
2024-11-16 21:20:03 +0800

    LOGIN/EMPLOYEEMODEL/USERMODEL/LOGINBLADE/REGISTRARBLADE/WEB(test)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginAuth;
 
 Route::get('/', function (){
     return view('login');
 });
+
+Route::post('/login', [LoginAuth::class, 'Login'])->name('login');
