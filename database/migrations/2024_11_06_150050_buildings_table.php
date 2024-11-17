aa8048ab29e32ad502a74d7526d5bba8a621cb0a
Joreen <villsanjor@gmail.com>
2024-11-17 18:27:56 +0800

    building database updates

--- database/migrations/2024_11_06_150050_buildings_table.php
+++ database/migrations/2024_11_06_150050_buildings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('buildings', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->nullable();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
