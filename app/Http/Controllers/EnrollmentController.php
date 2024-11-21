6d0c650ce40ab594dd0fb1163fb2e4de786050ca
sundei2 <sund3igarcia@gmail.com>
2024-11-21 18:41:06 +0800

    fixed the enrollment routes/views

--- app/Http/Controllers/EnrollmentController.php
+++ app/Http/Controllers/EnrollmentController.php
@@ -16,7 +16,7 @@ class EnrollmentController extends Controller
             })
             ->get();
 
-        return view('enrollment', compact('users'));
+        return view('enrollment.enrollment', compact('users'));
     }
 
     public function store(Request $request)
@@ -41,7 +41,7 @@ class EnrollmentController extends Controller
 
         $enrollments = Enrollment::with('user')->get();
 
-        return view('enrollmentTable', compact('enrollments'));
+        return view('enrollment.enrollmentTable', compact('enrollments'));
     }
 
     public function showNotEnrollmentTable()
@@ -55,7 +55,7 @@ class EnrollmentController extends Controller
 
     public function edit(Enrollment $enrollment)
     {
-        return view('enrollmentEditTable', compact('enrollment'));
+        return view('enrollment.enrollmentEditTable', compact('enrollment'));
     }
 
     public function update(Request $request, Enrollment $enrollment)
