3fa3ade7160167ef19b22e3d60da9a38ca0e8503
Joreen Santiago <124236284+rinfeysntg@users.noreply.github.com>
2024-12-08 21:41:45 +0800

    added payroll route to treasury dashboard

--- resources/views/treasury.blade.php
+++ resources/views/treasury.blade.php
@@ -6,7 +6,7 @@
         <div class="button-container">
         <a href="{{ route('announcements.announcement') }}"><button class="btn">Announcements</button></a>
         <a href="{{ route('payments.index') }}"><button class="btn">Payments</button></a>
-        <a href="{{ route('enrollment_dashboard') }}"><button class="btn">Payroll</button></a>
+        <a href="{{ route('payrolls') }}"><button class="btn">Payroll</button></a>
         <br>
         <div class="button-container-red">
         <a href="{{ route('logout') }}"><button class="btn">Log Out</button></a>
