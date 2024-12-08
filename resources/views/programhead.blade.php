d927179225ef324f265d2984078087cf05d03b2e
Joreen Santiago <124236284+rinfeysntg@users.noreply.github.com>
2024-12-08 18:04:12 +0800

    added announcement route to dashboards

--- resources/views/programhead.blade.php
+++ resources/views/programhead.blade.php
@@ -4,7 +4,7 @@
 <div class="glass">
         <img src="../images/wupLogo.png" alt="meh" class="dashboardLogo">
         <div class="button-container">
-        <a href="/announcement"><button class="btn">Announcements</button></a>
+        <a href="{{ route('announcements.announcement') }}"><button class="btn">Announcements</button></a>
         <a href="{{ route('enrollDashboard') }}"><button class="btn">Rooms</button></a>
         <a href="{{ route('students_index') }}"><button class="btn">Students</button></a>
         <a href="{{ route('curriculums_program_head') }}"><button class="btn">Schedule</button></a>
